9e8d3a8a52d4e4fda8449a86a2b0a0981fff014d
gamjatwigim <mbh930312@naver.com>
2019-01-22 22:36:49 +0900

    first commit

--- app/src/main/java/hbs/com/mvvm_dagger/MainActivity.kt
+++ app/src/main/java/hbs/com/mvvm_dagger/MainActivity.kt
@@ -3,13 +3,13 @@ package hbs.com.mvvm_dagger
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import hbs.com.mvvm_dagger.MyAnroidApplicationClass.Companion.SUPER_DATA
+import hbs.com.mvvm_dagger.MyAndroidApplicationClass.Companion.SUPER_DATA
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myApplication = application as MyAnroidApplicationClass
+        val myApplication = application as MyAndroidApplicationClass
         myApplication.sureData()
         Log.d("",SUPER_DATA.toString())
     }
