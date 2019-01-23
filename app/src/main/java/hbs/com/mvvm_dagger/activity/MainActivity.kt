e0571385f40b85d10482a90232dc04b055c6da00
gamjatwigim <mbh930312@naver.com>
2019-01-23 22:42:18 +0900

    issue02 migrate androidx

--- app/src/main/java/hbs/com/mvvm_dagger/activity/MainActivity.kt
+++ app/src/main/java/hbs/com/mvvm_dagger/activity/MainActivity.kt
@@ -1,14 +1,18 @@
-package hbs.com.mvvm_dagger
+package hbs.com.mvvm_dagger.activity
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import dagger.android.AndroidInjection
+import hbs.com.mvvm_dagger.MyAndroidApplicationClass
 import hbs.com.mvvm_dagger.MyAndroidApplicationClass.Companion.SUPER_DATA
+import hbs.com.mvvm_dagger.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AndroidInjection.inject(this)
         val myApplication = application as MyAndroidApplicationClass
         myApplication.sureData()
         Log.d("",SUPER_DATA.toString())
